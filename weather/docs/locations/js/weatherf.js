d195f934dcba4515cdc3f111aaddd93d522f7730
davidbobadilla.github.io <bob17002@byui.edu>
2019-10-12 17:56:40 -0600

    csd

---
+++ weather/docs/locations/js/weatherf.js
@@ -0,0 +1,18 @@
+function current() {
+    var todaysDate = new Date();
+    const longDayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayName = longDayNames[todaysDate.getDay()];
+    const longMonthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var monthName = longMonthNames[todaysDate.getMonth()];
+    document.getElementById("current").innerHTML = dayName + ", " + todaysDate.getDate() + " " + monthName + " " + todaysDate.getFullYear();
+}
+//gets the curreent year and displays the copyright info
+function copyright() {
+    var todaysDate = new Date();
+    var current = todaysDate.getFullYear();
+    document.getElementById('copyright').innerHTML = "&copy; " + current;
+}
+
+//calls the listed funciton after page has loaded
+window.onload = current();
+window.onload = copyright();
