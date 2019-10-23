86382d700fa1d1e796bc0841e2e5f6bf716c11fc
davidbobadilla <55507217+davidbobadilla@users.noreply.github.com>
2019-10-23 16:51:54 -0600

    Update weather-images.js

--- js/weather-images.js
+++ js/weather-images.js
@@ -1,1 +1,4 @@
-
+<!-- this will make the html to load first-->
+<script src="app.js" defer></script>
+<!-- place holder--> 
+<img src='data/img/placeholder.png' data-src='data/img/SLUG.jpg' alt='NAME'>
