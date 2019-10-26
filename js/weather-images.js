64f89f78d1cbdf253b4fb023efc476d5a7b5e47a
davidbobadilla.github.io <bob17002@byui.edu>
2019-10-25 22:21:18 -0600

    ds

--- js/weather-images.js
+++ js/weather-images.js
@@ -16,56 +16,61 @@
 // imagesToLoad.forEach((img) => {
 //   loadImages(img);
 // });
-const images = document.querySelectorAll("[data-src]");
-
-function preloadImage(img) {
-  const src = img.getAttribute("data-src");
-  if (!src) {
-    return;
-  }
-  img.src = src;
-}
-
-const imgOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px 200px 0px"
-};
-
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      preloadImage(entry.target);
-      imgObserver.unobserve(entry.target);
-    }
-  })
-}, imgOptions);
-
-images.forEach(image => {
-  imgObserver.observe(image);
-});
 
 
+
+
+//Josh
+// const images = document.querySelectorAll("[data-src]");
+
+// function preloadImage(img) {
+//   const src = img.getAttribute("data-src");
+//   if (!src) {
+//     return;
+//   }
+//   img.src = src;
+// }
+
+// const imgOptions = {
+//   threshold: 1,
+//   rootMargin: "0px 0px 200px 0px"
+// };
+
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//   entries.forEach(entry => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     } else {
+//       preloadImage(entry.target);
+//       imgObserver.unobserve(entry.target);
+//     }
+//   })
+// }, imgOptions);
+
+// images.forEach(image => {
+//   imgObserver.observe(image);
+// });
+
+//end josh
 
 //This one is the good one
-// if('IntersectionObserver' in window) {
-//   const observer = new IntersectionObserver((items, observer) => {
-//     items.forEach((item) => {
-//       if(item.isIntersecting) {
-//         loadImages(item.target);
-//         observer.unobserve(item.target);
-//       }
-//     });
-//   });
-//   imagesToLoad.forEach((img) => {
-//     observer.observe(img);
-//   });
-// } else {
-//   imagesToLoad.forEach((img) => {
-//     loadImages(img);
-//   });
-// }
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
 
 
 
