16a090d29aeb5747fd2561c6a6c8a245578b07a3
davidbobadilla.github.io <bob17002@byui.edu>
2019-10-25 23:07:26 -0600

    ds

--- js/weather-images.js
+++ js/weather-images.js
@@ -114,22 +114,22 @@
 
 
 
-const photos = document.querySelectorAll("[data-src]");
+let photos = document.querySelectorAll("[data-src]");
 
 function preloadImage(img) {
-  const src = img.getAttribute("data-src");
+  let src = img.getAttribute("data-src");
   if (!src) {
     return;
   }
   img.src = src;
 }
 
-const imgOptions = {
+let imgOptions = {
   threshold: 1,
   rootMargin: "0px 0px 200px 0px"
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+let imgObserver = new IntersectionObserver((entries, imgObserver) => {
   entries.forEach(entry => {
     if (!entry.isIntersecting) {
       return;
