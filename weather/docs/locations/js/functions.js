724c4c821bda7dd48d9edfedb1bd4743357b327f
davidbobadilla <55507217+davidbobadilla@users.noreply.github.com>
2019-10-31 10:17:41 -0600

    Update functions.js

--- weather/docs/locations/js/functions.js
+++ weather/docs/locations/js/functions.js
@@ -3,3 +3,24 @@
 // Weather Site JavaScript Functions
 
 console.log('My javascript is being read.');
+
+//This was moved from weatherf.js//
+
+function current() {
+    var todaysDate = new Date();
+    const longDayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayName = longDayNames[todaysDate.getDay()];
+    const longMonthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var monthName = longMonthNames[todaysDate.getMonth()];
+    document.getElementById("current").innerHTML = dayName + ", " + todaysDate.getDate() + " " + monthName + " " + todaysDate.getFullYear();
+}
+
+function copyright() {
+    var todaysDate = new Date();
+    var current = todaysDate.getFullYear();
+    document.getElementById('copyright').innerHTML = "&copy; " + current;
+}
+
+
+window.onload = current();
+window.onload = copyright();
