d45e61b260c44f2a4d2457fab226a109d879d210
davidbobadilla.github.io <bob17002@byui.edu>
2019-11-01 14:25:30 -0600

    hj

--- weather/docs/locations/js/functions.js
+++ weather/docs/locations/js/functions.js
@@ -44,18 +44,18 @@ window.onload = copyright();
 
 //Calculate windchill
 function buildWC(speed, temp) {
-let feels = document.getElementById('wind');
+let feels = document.getElementById("wind");
 
 // Compute the windchill
 let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-console.log(wc);
+console.log(`The wind is: ${wc}`);
 
 // Round the answer down to integer
 wc = Math.floor(wc);
 
 // If chill is greater than temp, return the temp
-wc = (wc > temp)?temp:wc;
-console.log(`Feels like: ${wc}`); // log wc to console
-  feels.innerHTML = wc; // output wc to page
+wc = (wc > temp) ? temp:wc;
+console.log(`The wind is: ${wc}`); // log wc to console
+feels.innerHTML = wc; // output wc to page
 
 }
