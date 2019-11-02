2a74cc851e460cfe8735ff72055d96d34c09db12
davidbobadilla.github.io <bob17002@byui.edu>
2019-11-01 21:22:04 -0600

    ds

--- weather/docs/locations/js/functions.js
+++ weather/docs/locations/js/functions.js
@@ -47,40 +47,33 @@ window.onload = copyright();
 
 
 
-// //Calculate windchill
+function buildWC(speed, temp){
+    let feelTemp = document.getElementById("chill"); // grab the span used to hold the wc
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16); 
+    console.log(`The wind chill is: ${wc}`); // log wc to console
+    wc = Math.floor(wc); 
+    wc = (wc > temp) ? temp:wc; 
+    console.log(`The wind chill is: ${wc}`); // log wc to console
+    feelTemp.innerHTML = wc; // output wc 
+  }
+
+
+// //mine Calculate the Windchill
 // function buildWC(speed, temp) {
-// let feels = document.getElementById("wind");
-
-// // Compute the windchill
-// let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-// console.log(`The wind is: ${wc}`);
-
-// // Round the answer down to integer
-// wc = Math.floor(wc);
-
-// // If chill is greater than temp, return the temp
-// wc = (wc > temp) ? temp:wc;
-// console.log(`The wind is: ${wc}`); // log wc to console
-// feels.innerHTML = wc; // output wc to page
-// }
-
-
-// Calculate the Windchill
-function buildWC(speed, temp) {
-    let feelTemp = document.getElementById('chills');
+//     let feelTemp = document.getElementById('chills');
    
-    // Compute the windchill
-    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-    console.log(wc);
+//     // Compute the windchill
+//     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+//     console.log(wc);
    
-    // Round the answer down to integer
-    wc = Math.floor(wc);
+//     // Round the answer down to integer
+//     wc = Math.floor(wc);
    
-    // If chill is greater than temp, return the temp
-    wc = (wc > temp)?temp:wc;
+//     // If chill is greater than temp, return the temp
+//     wc = (wc > temp)?temp:wc;
    
-    // Display the windchill
-    console.log(wc);
-    // wc = 'Feels like '+wc+'°F';
-    feelTemp.innerHTML = wc;
-    }
+//     // Display the windchill
+//     console.log(wc);
+//     // wc = 'Feels like '+wc+'°F';
+//     feelTemp.innerHTML = wc;
+//     }
