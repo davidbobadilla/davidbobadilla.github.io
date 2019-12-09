a1fae0b9973e55d8570f7baa6fd88ee15f3e05b0
davidbobadilla.github.io <bob17002@byui.edu>
2019-12-09 12:22:33 -0700

    ds

--- temple-inn/js/functionstemple.js
+++ temple-inn/js/functionstemple.js
@@ -27,3 +27,44 @@ function copyright() {
 
 window.onload = current();
 window.onload = copyright();
+
+
+
+// Start of form for the reservation page
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.querySelector('#submit').addEventListener("click", processData);
+  })
+  
+  let reservations = [];
+  let processData = (event) => {
+    // stop the form from submitting
+    event.preventDefault();
+    let reservation = {
+    guests: document.querySelector('#guests').value,
+    resDate: document.querySelector('#resDate').value
+    }
+  // adds reservation to the end of the array of all reservations
+  reservations.push(reservation);
+  // reset the first, and only, form
+  document.forms[0].reset;
+  // see results in console
+  console.log('newRes', {reservations});
+  
+  
+    // Store to session Storage
+    window.sessionStorage.setItem("reservations", JSON.stringify(reservations));
+    // Retrieve from session storage
+    let resList = JSON.parse(window.sessionStorage.getItem("reservations"));
+    console.log(resList);
+    
+    // inject to the page
+  const resDetails = document.querySelector("#resResult pre");
+  resDetails.textContent = "\n" + JSON.stringify(reservations, "\t", 2);
+    
+  // display the results
+  document.querySelector("#resResult").classList.remove("hide");
+  
+  }
+
+// end reservation form 
