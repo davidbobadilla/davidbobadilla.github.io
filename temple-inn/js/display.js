133471b3bc5e96bc1e72f9c7ca7a0d848875299c
davidbobadilla.github.io <bob17002@byui.edu>
2019-12-14 14:19:06 -0700

    ds

--- temple-inn/js/display.js
+++ temple-inn/js/display.js
@@ -3,16 +3,14 @@ var $ = document.querySelector.bind(document);
 
 buildReservationConfirmation();
 function buildReservationConfirmation() {
-    console.log("Im being read");
-  $("#fname").innerHTML = "Name: " + sessStore.getItem('firstName') + " " + sessStore.getItem('lastName');
-//   $("#Email").innerHTML = "Email Address: " + reservation.emailAddress;
-//   $("#phone").innerHTML = "Phone Number: " + reservation.phoneNumber;
-//   $("#city").innerHTML = "City and State: " + reservation.location ;
-//   $("#guests").innerHTML = "# of Guest: " + reservation.guest;
-//   $("#resDate").innerHTML = "Check-in Date: " + reservation.checkInDate;
-//   $("#resDate1").innerHTML ="Check-out Date: " + reservation.checkOutDate;
-//   $("#subject1").innerHTML = "Additional Comments: " + reservation.additionalComments;
-
-
+    console.log("Reading");
+  $('#fname').innerHTML = "Name: " + sessStore.getItem('firstName: ') + " " + sessStore.getItem('lastName: ');
+  $('#Email').innerHTML = "Email Address: " + sessStore.getItem('emailAddress: ');
+  $('#phone').innerHTML = "Phone Number: " + sessStore.getItem('phoneNumber: ');
+  $('#city').innerHTML = "City and State: " + sessStore.getItem('location: ');
+  $('#guests').innerHTML = "# of Guest: " + sessStore.getItem('guest: ');
+  $('#resDate').innerHTML = "Check-in Date: " + sessStore.getItem('checkInDate: ');
+  $('#resDate1').innerHTML = "Check-out Date: " + sessStore.getItem('checkOutDate: ');
+  $('#subject1').innerHTML = "Additional Comments: " + sessStore.getItem('additionalComments: ');
 
 }
